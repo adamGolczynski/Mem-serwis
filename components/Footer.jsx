2a937ffb108da6c058b04af74a91254c7db50315
maadzjaa <magda.g2112@gmail.com>
2024-05-18 19:28:06 +0200

    feat: add footer

---
+++ components/Footer.jsx
@@ -0,0 +1,9 @@
+import styles from '@/components/Footer.module.css';
+
+export default function Footer() {
+	return (
+		<footer className={styles.footer}>
+			<p>&copy; {new Date().getFullYear()} M&A Websites. All rights reserved. </p>
+		</footer>
+	);
+}
