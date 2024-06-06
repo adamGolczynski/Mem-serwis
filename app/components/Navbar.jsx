963bb971c6da647d98979efd5ce2afa8e7d0c0d5
adamGolczynski <agolczynski97@gmail.com>
2024-06-06 20:15:30 +0200

    style: nav and fotter

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -1,30 +1,42 @@
-'use client';
-
-import { usePathname } from 'next/navigation';
-import Link from 'next/link';
-import Image from 'next/image';
-import Logo from './mem_logo.png';
-import styles from './Navbar.module.css';
+'use client'
+import Link from 'next/link'
+import Image from 'next/image'
+import Logo from './mem_logo.png'
+import styles from './Navbar.module.css'
+import { usePathname } from 'next/navigation'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBars, faX } from '@fortawesome/free-solid-svg-icons'
 
 export function Navbar() {
-	const pathname = usePathname();
+	const pathname = usePathname()
 
 	return (
-		<nav className={styles.navbar}>
-			<Image src={Logo} alt='Mem serwis logo' width={70} quality={100} placeholder='blur' />
-			<h3>Mem serwis</h3>
-			<Link className={`link ${pathname === '/' ? 'active' : ''}`} href='/'>
-				Home
-			</Link>
-			<Link className={`link ${pathname === '/regular' ? 'active' : ''}`} href='/regular'>
-				Regular
-			</Link>
-			<Link className={`link ${pathname === '/hot' ? 'active' : ''}`} href='/hot'>
-				Hot
-			</Link>
-			<Link className={`link ${pathname === '/formMem' ? 'active' : ''}`} href='/formMem'>
-				Add mem
-			</Link>
+		<nav className={styles.navBar}>
+			<input type='checkbox' id='sidebar-active' className={styles.sidebarActive}/>
+			<label htmlFor='sidebar-active' className={styles.openSidebarButton}>
+				<FontAwesomeIcon icon={faBars} className={styles.iconStyle} />
+			</label>
+
+			<label id='overlay' htmlFor="sidebar-active" className={styles.overlay}></label>
+
+			<div className={styles.linksContainer}>
+				<label htmlFor='sidebar-active' className={styles.closeSidebarButton}>
+					<FontAwesomeIcon icon={faX} className={styles.iconStyle} />
+				</label>
+				<Link className={`${styles.homeLink} ${styles.link}`} href='/'>
+				<Image src={Logo} alt='Mem serwis logo' width={60} quality={100} placeholder='blur' className={styles.logo} />
+				<h3 className={styles.title}>Memizer</h3>
+				</Link>
+				<Link className={`${styles.link} ${pathname === '/regular' ? styles.active : ''}`} href='/regular'>
+					Regular
+				</Link>
+				<Link className={`${styles.link} ${pathname === '/hot' ? styles.active : ''}`} href='/hot'>
+					Hot
+				</Link>
+				<Link className={`${styles.link} ${pathname === '/formMem' ? styles.active : ''}`} href='/formMem'>
+					Add mem
+				</Link>
+			</div>
 		</nav>
-	);
+	)
 }
