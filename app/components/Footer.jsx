3b79c63efc021c97a524b655ce2d8e133d8d83d8
adamGolczynski <agolczynski97@gmail.com>
2024-06-08 17:18:34 +0200

    add: github icon in footer

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -1,9 +1,14 @@
-import styles from '@/app/components/Footer.module.css';
+import styles from '@/app/components/Footer.module.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faGithub } from '@fortawesome/free-brands-svg-icons'
 
 export default function Footer() {
 	return (
 		<footer className={styles.footer}>
 			<p>&copy; {new Date().getFullYear()} M&A Websites. All rights reserved. </p>
+			<a href='https://github.com/adamGolczynski/Mem-serwis' target='_blank' rel='noopener noreferrer'>
+				<FontAwesomeIcon className={styles.icon} icon={faGithub} />
+			</a>
 		</footer>
-	);
+	)
 }
